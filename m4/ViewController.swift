a35954b6685952c725f118ee8eadce64e6884f0c
TessHutchinson <ekutessa@gmail.com>
2026-06-14 19:27:05 -0400

    Initial commit

---
+++ m4/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  m4
+//
+//  Created by Vanessa.Hutchinson on 2026-06-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var moodSlider: UISlider!
+    @IBOutlet weak var moodLabel: UILabel!
+    @IBOutlet weak var moodDatePicker: UIDatePicker!
+    @IBOutlet weak var savedEntryLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateMoodLabel(for: moodSlider.value)
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        updateMoodLabel(for: sender.value)
+    }
+    
+    func updateMoodLabel(for value: Float) {
+        let moodValue = Int(value)
+        
+        switch moodValue {
+        case 0...20:
+            moodLabel.text = "Very Sad \u{1F622}"
+        case 21...40:
+            moodLabel.text = "Sad "
+        case 41...60:
+            moodLabel.text = "Neutral"
+        case 61...80:
+            moodLabel.text = "Happy"
+        default:
+            moodLabel.text = "Very Happy"
+        }
+    }
+    
+    @IBAction func saveMoodTapped(_ sender: UIButton) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d"
+        let dateString = dateFormatter.string(from: moodDatePicker.date)
+        
+        let emoji = moodLabel.text?.split(separator: " ").last ?? ""
+        
+        savedEntryLabel.text = "on \(dateString), you felt \(emoji)"
+    }
+
+
+}
+
---
+++ m4Tests/m4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  m4Tests.swift
+//  m4Tests
+//
+//  Created by Vanessa.Hutchinson on 2026-06-14.
+//
+
+import Testing
+@testable import m4
+
+struct m4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
